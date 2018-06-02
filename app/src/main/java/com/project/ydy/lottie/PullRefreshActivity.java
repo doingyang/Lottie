1f8816d79c69269c3f45228d39cdc215c70bd3b2
ydy1102 <yudongyang@tzt.cn>
2018-06-02 16:19:07 +0800

    添加一些依赖

--- app/src/main/java/com/project/ydy/lottie/PullRefreshActivity.java
+++ app/src/main/java/com/project/ydy/lottie/PullRefreshActivity.java
@@ -32,10 +32,22 @@ public class PullRefreshActivity extends AppCompatActivity implements PtrHandler
         this.mPtr.setHeaderView(krHeader);
         this.mPtr.addPtrUIHandler(krHeader);
         this.mPtr.setPtrHandler(this);
-        this.mPtr.setDurationToCloseHeader(1000);
 
-        this.mPtr.setDurationToClose(500);
+        //阻尼系数：默认1.7f，越大，感觉下拉时越吃力
+        this.mPtr.setResistance(1.7f);
+        //触发刷新时移动的位置比例：默认1.2f，移动达到头部高度1.2倍时可触发刷新操作
+        this.mPtr.setRatioOfHeaderHeightToRefresh(1.2f);
+        //回弹延时：默认200ms，回弹到刷新高度所用时间
+        this.mPtr.setDurationToClose(200);
+        //最短加载时间
         this.mPtr.setLoadingMinTime(1500);
+        //头部回弹时间：刷新完成，关闭Header的时间，默认1000ms
+        this.mPtr.setDurationToCloseHeader(1500);
+        //刷新时是否保持头部：默认值true
+        this.mPtr.setKeepHeaderWhenRefresh(true);
+        //下拉开始刷新还是释放后再开始刷新：默认为false释放刷新
+        this.mPtr.setPullToRefresh(false);
+
         this.mPtr.setEnabledNextPtrAtOnce(true);
     }
 
